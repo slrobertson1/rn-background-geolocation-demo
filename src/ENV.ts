00370db096b1bdd2c811847fef1af2e8579800f4
Robertson, Scott Lee <scott.robertson@imtc.gatech.edu>
2020-10-26 12:30:52 -0400

    User HTTPS for tracker host

--- src/ENV.ts
+++ src/ENV.ts
@@ -1,5 +1,5 @@
 const ENV = {
-	TRACKER_HOST: 'http://can-sensor.imtc.gatech.edu'
+	TRACKER_HOST: 'https://can-sensor.imtc.gatech.edu'
 	// TRACKER_HOST: 'http://10.0.1.234'
 	// TRACKER_HOST: 'http://tracker.transistorsoft.com'
 };
